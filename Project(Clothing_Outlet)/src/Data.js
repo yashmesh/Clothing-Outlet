2857b191c1597299df185daae4a3dd553c458e92
yashmesh <109825455+yashmesh@users.noreply.github.com>
2022-07-30 15:53:37 +0530

    Add files via upload

---
+++ Project(Clothing_Outlet)/src/Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Casual Shirt",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img1.jpg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Office Shirt",
+    price: 50,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img2.jpg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "T Shirt",
+    price: 25,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img3.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Mens Suit",
+    price: 30,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img4.jpg",
+  },
+];
